202308bd5c567c59011316cb4dce0283d5b01174
Ollie James <oliver.james@candide.eu>
2021-02-24 22:17:25 +0000

    Add gql schema fetching + type generation

---
+++ src/generated/graphql.ts
@@ -0,0 +1,30 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: MyFileQuery
+// ====================================================
+
+export interface MyFileQuery_files {
+  __typename: "File";
+  name: string;
+}
+
+export interface MyFileQuery {
+  files: (MyFileQuery_files | null)[];
+}
+
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,15 @@
+import { gql } from "@apollo/client";
 import { ChakraProvider, Text } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom";
+
+const testQuery = gql`
+  query MyFileQuery {
+    files {
+      name
+    }
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
