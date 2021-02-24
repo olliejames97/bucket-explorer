1120bec26d110351b9f531dc1d3733367d8af157
Ollie James <oliver.james@candide.eu>
2021-02-24 22:08:16 +0000

    Add chakra-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
+import { ChakraProvider, Text } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <p>Hello world</p>
+    <ChakraProvider>
+      <Text>Hello chakra</Text>
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
